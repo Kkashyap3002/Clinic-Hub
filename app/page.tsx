8b2e02f7e354772db2cccddec8c0476b3e398041
Kishan Kashyap <Kashyap3002@gmail.com>
2025-01-22 23:46:58 +0530

    Update Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/ui/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
-      <Button> Click Me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex items-center">
+            <Image
+              src="/assets/icons/full-logo.svg"
+              width={1050}
+              height={1050}
+              alt="patient"
+              className="mb-12 h-12 w-fit rounded-2xl roboto-font"
+            />
+            <span className="flex items-center justify-center mb-12 h-12 w-fit font text-24-bold pl-[3.5px] text-white">
+              ClinicHub
+            </span>
+          </div>
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 CinicHub. All Rights Reserved.
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        width={1000}
+        height={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
-  )
+  );
 }
